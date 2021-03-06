5d545c0213c8fded5e8f226be2187fdc0f5cb1d5
Fco. Javier Mesa Olmo <francisco.mesa@ebury.com>
2021-03-06 21:54:45 +0100

    feat: Create endpoint to get account details by id

--- backend/src/accounts/accounts.service.ts
+++ backend/src/accounts/accounts.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
 
-import { Account } from '../interfaces/Account'
+import { Account, AccountDetails } from '../interfaces/Account'
+
+import { generateRandomBalance, generateRandomOrderId, generateRandomOrderCode } from '../helpers/random-characters'
 
 @Injectable()
 export class AccountsService {
-
-  getRandomNumber(): Number {
-    return Math.floor(Math.random() * 5000)/100;
-  }
 
   getAccounts(): Account[] {
     let accounts = [];
     for (let i = 0; i <15; i++) {
       accounts.push({
+        id: i,
         name: 'Testing account_' + i,
         category: 'affiliates',
         tags: 'test',
-        balance: this.getRandomNumber(),
-        available_balance: this.getRandomNumber()
+        balance: generateRandomBalance(),
+        available_balance: generateRandomBalance()
       });
     }
 
     return accounts;
+  }
+
+  getAccountById(id): AccountDetails[] {
+    let statements = [];
+    for (let i = 0; i <5; i++) {
+      statements.push({
+        id: id,
+        confirmed_date: '02/11/2020 14:45',
+        order_id: generateRandomOrderId(),
+        order_code: generateRandomOrderCode(),
+        transaction_type: 'Payment received',
+        debit: 0,
+        credit: generateRandomBalance(),
+        balance: generateRandomBalance()
+      });
+    }
+
+    return statements;
   }
 }
--- backend/src/interfaces/Account.ts
+++ backend/src/interfaces/Account.ts
@@ -1,7 +1,19 @@
 export interface Account {
+  id: number,
   account_name: string,
   category: string,
   tags: string,
   balance: number,
   available_balance: number
+}
+
+export interface AccountDetails {
+  id: number,
+  confirmed_date: string,
+  order_id: string,
+  order_code: string,
+  transaction_type: string,
+  debit: number,
+  credit: number,
+  balance: number
 }
